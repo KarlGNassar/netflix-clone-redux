8a916eb473e9bb9fcc7f5768dad544d86e67ed6c
KARL NASSAR <kgnassar01@gmail.com>
2021-02-05 00:30:59 +0200

    cleanup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDnYEUsMcEByV0nIE4eZRf5Ru8YMb_x0c",
+    authDomain: "netflix-2774d.firebaseapp.com",
+    projectId: "netflix-2774d",
+    storageBucket: "netflix-2774d.appspot.com",
+    messagingSenderId: "891837101283",
+    appId: "1:891837101283:web:81c4267f31efda755301d6",
+    measurementId: "G-2JVCKJLTSH"
+  };
