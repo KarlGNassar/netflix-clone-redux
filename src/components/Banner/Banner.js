10d063eb5047fb6244b939a51e1deea5ce7aa084
KARL NASSAR <kgnassar01@gmail.com>
2021-02-10 20:28:47 +0200

    banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -27,7 +27,6 @@ const Banner = () => {
         <header className="banner" style={{
             backgroundSize: 'cover',
             backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
-            backgroundPosition: 'center',
         }}>
             <div className="banner__contents">
                 <h1 className="banner__title">
